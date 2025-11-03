83f77c865ecc072e0cfd5639b881fa7cf508e049
Arthur Simões <amsbsimoes@gmail.com>
2025-11-02 22:21:31 -0300

    add(JwtAuthFilter)

---
+++ src/main/java/com/DesafioTec/CheckList/Security/JwtAuthFilter.java
@@ -0,0 +1,53 @@
+package com.DesafioTec.CheckList.Security;
+
+import com.DesafioTec.CheckList.service.user.AuthService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil util;
+    private final AuthService service;
+
+    public JwtAuthFilter(JwtUtil util, AuthService service) {
+        this.util = util;
+        this.service = service;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+            String header = request.getHeader("Authorization");
+
+            if (header != null && header.startsWith("Bearer")){
+                String token = header.substring(7);
+                try {
+                    String username = util.getUsernameFromToken(token);
+                    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                        UserDetails userDetails = service.loadUserByUsername(username);
+
+                        UsernamePasswordAuthenticationToken authToken =
+                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
+                } catch (Exception e){
+                    throw new RuntimeException();
+                }
+            } filterChain.doFilter(request,response);
+    }
+}
---
+++ src/main/java/com/DesafioTec/CheckList/Security/JwtUtil.java
@@ -0,0 +1,56 @@
+package com.DesafioTec.CheckList.Security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class JwtUtil {
+    private final Key key;
+    private final long expirationMs;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret,
+                   @Value("${jwt.expiration-ms}") long expirationMs) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.expirationMs = expirationMs;
+    }
+
+    public String genereteToken(UUID userId, String username){
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + expirationMs);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("userId" , userId.toString())
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Jws<Claims> validateToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+    }
+
+    public UUID getUserIdFromToken(String token) {
+        Claims claims = validateToken(token).getBody();
+        String userId = claims.get("userId", String.class);
+        return UUID.fromString(userId);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return validateToken(token).getBody().getSubject();
+    }
+
+}
